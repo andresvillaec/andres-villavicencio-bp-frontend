b00319996635976e674d96f5d0a713494417c045
Andres Villavicencio <avillavicencio@devsu.com>
2024-10-20 21:51:50 -0500

    feat: Add route for products list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from "./features/products/product-list/product-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Listado de productos',
+    component: ProductListComponent,
+  },
+  {
+    path: 'products',
+    title: 'Listado de productos',
+    component: ProductListComponent,
+  },
+];
