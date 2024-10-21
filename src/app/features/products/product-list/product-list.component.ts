c2598e1ff04d05a84df8753752587b55ce09cdb0
Andres Villavicencio <avillavicencio@devsu.com>
2024-10-21 06:54:11 -0500

    feat: Get product list from server

--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from "../models/product.model";
 import { FormsModule } from '@angular/forms';
 import { DefaultDatePipe } from "../../../shared/pipes/default-date.pipe";
 import { SearchListService  } from "../../../shared/services/search-list.service";
+import { ProductService } from "../services/product.service";
+import { ProductResponse } from '../interfaces/product-response';
 
 @Component({
   selector: 'app-product-list',
@@ -12,54 +14,72 @@ import { SearchListService  } from "../../../shared/services/search-list.service
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   searchTerm: string = '';
   limit: number = 4;
+  products: Product[] = [];
+  errorMessage: string = ''; // Holds error messages
+  
+  constructor(private searchListService: SearchListService, private productService: ProductService) {}
 
-  constructor(private searchListService: SearchListService) {}
+  ngOnInit() : void {
+    this.loadProducts();
+  }
 
-  public products: Product[] = [
-    {
-      "id": "1",
-      "name": "Tarjeta de credito",
-      "description": "Adicional del Titular",
-      "logo": "logo.png",
-      "deliveryDate": new Date(2024, 11, 11),
-      "reviewDate": new Date(2025, 2, 26),
-    },
-    {
-      "id": "2",
-      "name": "Tarjeta de debito",
-      "description": "Solicitada por el cliente",
-      "logo": "logo.png",
-      "deliveryDate": new Date(2024, 11, 21),
-      "reviewDate": new Date(2025, 3, 15),
-    },
-    {
-      "id": "3",
-      "name": "Tarjeta de debito",
-      "description": "Reposición por perdida",
-      "logo": "logo.png",
-      "deliveryDate": new Date(2024, 12, 31),
-      "reviewDate": new Date(2025, 10, 15),
-    },
-    {
-      "id": "4",
-      "name": "Tarjeta de debito",
-      "description": "Reposición por perdida",
-      "logo": "logo.png",
-      "deliveryDate": new Date(2024, 12, 31),
-      "reviewDate": new Date(2025, 10, 15),
-    },
-    {
-      "id": "5",
-      "name": "Tarjeta de debito",
-      "description": "Reposición por perdida",
-      "logo": "logo.png",
-      "deliveryDate": new Date(2024, 12, 31),
-      "reviewDate": new Date(2025, 10, 15),
-    },
-  ];
+  loadProducts(): void {
+    this.productService.getProducts().subscribe({
+      next: (response) => {
+        this.products = response.data;  // Assigning fetched data to products array
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+        this.errorMessage = 'Error loading products';  // Handle errors
+      }
+    });
+  }
+
+  // public products: Product[] = [
+  //   {
+  //     "id": "1",
+  //     "name": "Tarjeta de credito",
+  //     "description": "Adicional del Titular",
+  //     "logo": "logo.png",
+  //     "deliveryDate": new Date(2024, 11, 11),
+  //     "reviewDate": new Date(2025, 2, 26),
+  //   },
+  //   {
+  //     "id": "2",
+  //     "name": "Tarjeta de debito",
+  //     "description": "Solicitada por el cliente",
+  //     "logo": "logo.png",
+  //     "deliveryDate": new Date(2024, 11, 21),
+  //     "reviewDate": new Date(2025, 3, 15),
+  //   },
+  //   {
+  //     "id": "3",
+  //     "name": "Tarjeta de debito",
+  //     "description": "Reposición por perdida",
+  //     "logo": "logo.png",
+  //     "deliveryDate": new Date(2024, 12, 31),
+  //     "reviewDate": new Date(2025, 10, 15),
+  //   },
+  //   {
+  //     "id": "4",
+  //     "name": "Tarjeta de debito",
+  //     "description": "Reposición por perdida",
+  //     "logo": "logo.png",
+  //     "deliveryDate": new Date(2024, 12, 31),
+  //     "reviewDate": new Date(2025, 10, 15),
+  //   },
+  //   {
+  //     "id": "5",
+  //     "name": "Tarjeta de debito",
+  //     "description": "Reposición por perdida",
+  //     "logo": "logo.png",
+  //     "deliveryDate": new Date(2024, 12, 31),
+  //     "reviewDate": new Date(2025, 10, 15),
+  //   },
+  // ];
 
   // Function to filter products based on search term
   get filteredProducts(): Product[] {
---
+++ src/app/features/products/services/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';  // Import HttpClient
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
+import { Product } from "../models/product.model";
+import { ProductResponse } from "../interfaces/product-response";
+
+@Injectable({
+  providedIn: 'root'  // Ensure this service is available globally
+})
+export class ProductService {
+  private apiUrl = 'http://localhost:3002/bp/products';  // API endpoint
+
+  constructor(private http: HttpClient) {}  // Inject HttpClient here
+
+  getProducts(): Observable<{ data: Product[] }> {
+    return this.http.get<{ data: Product[] }>(this.apiUrl); // Define the expected response type
+  }
+}
