b28a8e6f18fc4ebdf9596f988457d1d16fba104a
Andres Villavicencio <avillavicencio@devsu.com>
2024-10-20 23:12:56 -0500

    feat: Load product list and set default date

--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from "../models/product.model";
+import { DateFormat } from "../../../shared/constants";
 
 @Component({
   selector: 'app-product-list',
@@ -10,6 +11,15 @@ import { Product } from "../models/product.model";
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  public dateFormat = DateFormat;
   public products: Product[] = [
+    {
+      "id": "1",
+      "name":"Tarjeta de credito",
+      "description":"Adicional del Titular",
+      "logo":"logo.png",
+      "deliveryDate": new Date(2024, 11, 21),
+      "reviewDate": new Date(2025, 2, 15),
+    }
   ];
 }
