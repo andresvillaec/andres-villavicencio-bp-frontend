c7b903cfaa17a36098280423a937479a41031736
Andres Villavicencio <avillavicencio@devsu.com>
2024-10-21 13:28:29 -0500

    feat: Style edit product

--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -72,51 +72,8 @@ export class ProductListComponent implements OnInit {
 
   // Cancel the delete operation and hide the popup
   cancelDelete(): void {
-    this.showDeletePopup = false;  // Hide the confirmation popup
+    this.showDeletePopup = false;
   }
-
-  // public products: Product[] = [
-  //   {
-  //     "id": "1",
-  //     "name": "Tarjeta de credito",
-  //     "description": "Adicional del Titular",
-  //     "logo": "logo.png",
-  //     "deliveryDate": new Date(2024, 11, 11),
-  //     "reviewDate": new Date(2025, 2, 26),
-  //   },
-  //   {
-  //     "id": "2",
-  //     "name": "Tarjeta de debito",
-  //     "description": "Solicitada por el cliente",
-  //     "logo": "logo.png",
-  //     "deliveryDate": new Date(2024, 11, 21),
-  //     "reviewDate": new Date(2025, 3, 15),
-  //   },
-  //   {
-  //     "id": "3",
-  //     "name": "Tarjeta de debito",
-  //     "description": "Reposición por perdida",
-  //     "logo": "logo.png",
-  //     "deliveryDate": new Date(2024, 12, 31),
-  //     "reviewDate": new Date(2025, 10, 15),
-  //   },
-  //   {
-  //     "id": "4",
-  //     "name": "Tarjeta de debito",
-  //     "description": "Reposición por perdida",
-  //     "logo": "logo.png",
-  //     "deliveryDate": new Date(2024, 12, 31),
-  //     "reviewDate": new Date(2025, 10, 15),
-  //   },
-  //   {
-  //     "id": "5",
-  //     "name": "Tarjeta de debito",
-  //     "description": "Reposición por perdida",
-  //     "logo": "logo.png",
-  //     "deliveryDate": new Date(2024, 12, 31),
-  //     "reviewDate": new Date(2025, 10, 15),
-  //   },
-  // ];
 
   // Function to filter products based on search term
   get filteredProducts(): Product[] {
