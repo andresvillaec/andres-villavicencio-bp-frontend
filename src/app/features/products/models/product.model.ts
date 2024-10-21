b1f1d48b8c05f945f6089853f2cd9e8abfef3162
Andres Villavicencio <avillavicencio@devsu.com>
2024-10-20 22:36:19 -0500

    feat: Create table html

---
+++ src/app/features/products/models/product.model.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: string;
+  logo: string;
+  name: string;
+  description: string;
+  deliveryDate: Date;
+  reviewDate: Date;
+}
--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from "../models/product.model";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-
+  public products: Product[] = [
+  ];
 }
