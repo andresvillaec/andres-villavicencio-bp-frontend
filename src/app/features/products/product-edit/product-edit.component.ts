d40edacbac2e7bf02af41418f85bac0ef932d76f
Andres Villavicencio <avillavicencio@devsu.com>
2024-10-21 19:35:32 -0500

    feat: Implement reset form button

--- src/app/features/products/product-edit/product-edit.component.ts
+++ src/app/features/products/product-edit/product-edit.component.ts
@@ -92,6 +92,10 @@ export class ProductEditComponent {
     }
   }
 
+  resetForm(): void {
+    this.productForm.reset();
+  }
+
   onSubmit(): void {
     if (this.productForm.valid) {
       const updatedProduct: Product = {
